53dab9d0623c39c56a15996c26d9ed6b98aa6f6e
Karolina Kolcova <Karolina.Kolcova@inbox.lv>
2021-04-21 16:15:14 +0300

    Notes and AllWeekNotes changes(diagnostics).

--- src/main/java/AllWeekNotes.java
+++ src/main/java/AllWeekNotes.java
@@ -6,19 +6,19 @@ import java.util.List;
 
 public class AllWeekNotes {
     public static void main(String[] args) {
-        MainNote call = new MainNote(Notes.Duplex, 20);
+        MainNote diagnostics = new MainNote(Notes.DUPLEX, 20);
 
         //System.out.println(call.getName());
-        System.out.println("This notes is: " + call.getName());
+        System.out.println("This notes is: " + diagnostics.getName());
 
-        call.addPatient(new Patient("Dmitrijs", "DM.", "dima@dm.lv"));
-        call.addPatient(new Patient("Nikolajs", "Pops", "nik@po.lv"));
-        call.addPatient(new Patient("Marite", "Broka", "marite@br.lv"));
+        diagnostics.addPatient(new Patient("Dmitrijs", "DM.", "dima@dm.lv"));
+        diagnostics.addPatient(new Patient("Nikolajs", "Pops", "nik@po.lv"));
+        diagnostics.addPatient(new Patient("Marite", "Broka", "marite@br.lv"));
 
         //System.out.println(call.getPatients().size());
         //  System.out.println(call.getPatients().get(0).getFirstName());
 
-        List<Patient> patients = call.getPatients();
+        List<Patient> patients = diagnostics.getPatients();
 
         for (int i = 0; i < patients.size(); i++) {
             System.out.println(patients.get(i).getFirstName() + " " + patients.get(i).getLastName());
--- src/main/java/model/enums/Notes.java
+++ src/main/java/model/enums/Notes.java
@@ -1,6 +1,19 @@
 package model.enums;
 
 public enum Notes {
-    Duplex, EHO, FGS
+    DUPLEX("Ultrasound non-invasive diagnostic"),
+    EHO("Echocardiographi"),
+    FGS("Fibrogastroscopy");
+
+    private String name;
+
+    Notes (String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
 
 }
